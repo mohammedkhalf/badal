07d4fb13b4b0bbc8044bcf2f748e7dcd2deba0e1
Sahla-Agency <sahla.agency@gmail.com>
2022-07-03 12:13:40 +0200

    fix widget count in DB

--- platform/core/dashboard/resources/views/widgets/stats.blade.php
+++ platform/core/dashboard/resources/views/widgets/stats.blade.php
@@ -1,14 +1,14 @@
 @if (empty($widgetSetting) || $widgetSetting->status == 1)
     <div class="col-lg-3 col-md-3 col-sm-6 col-12">
-        <a class="dashboard-stat dashboard-stat-v2" style="background-color: {{ $widget->color }}; color: #fff" href="{{--{{ $widget->route }}--}}">
+        <a class="dashboard-stat dashboard-stat-v2" style="background-color: {{ $widget->color }}; color: #fff" href="{{ $widget->route }}">
             <div class="visual">
                 <i class="{{ $widget->icon }}" style="opacity: .1;"></i>
             </div>
             <div class="details">
                 <div class="number">
-                    <span data-counter="counterup" data-value="{{--{{ $widget->statsTotal }}--}}">0</span>
+                    <span data-counter="counterup" data-value="{{ $widget->statsTotal }}">0</span>
                 </div>
-                <div class="desc"> {{--{{ $widget->title }} --}} </div>
+                <div class="desc"> {{ $widget->title }}  </div>
             </div>
         </a>
     </div>
